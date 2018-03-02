0c74bc2e07e3387affbfcc6beb8c38d603c2b5f5
weiguo.ni <neefrankie@gmail.com>
2018-03-02 18:23:16 +0800

    test

--- test/validate.js
+++ test/validate.js
@@ -4,10 +4,10 @@ const schema = Joi.object().keys({
   username: Joi.string().alphanum().min(3).max(30).required(),
   password: Joi.string().regex(/^[a-zA-Z0-9]{3,30}$/),
   access_token: [Joi.string(), Joi.number()],
-  birthyear: Joi.number().integer().min(1900).max(2013),
+  birthyear: Joi.number().integer().min(1900).max(2013).required(),
   email: Joi.string().email()
 });
 
-const result = Joi.validate({username: 'ab', birthyear: 1994}, schema);
+const result = Joi.validate({username: 'ab', birthyear: 3000}, schema);
 
 console.log("%o", result)
