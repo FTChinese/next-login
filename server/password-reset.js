87d87cc19e461b3dae039c0c968f5703bc17ba8e
weiguo.ni <neefrankie@gmail.com>
2018-03-08 23:11:18 +0800

    use keyv as ttl cache

--- server/password-reset.js
+++ server/password-reset.js
@@ -1,6 +1,7 @@
 const debug = require('debug')('user:password-reset');
 const Router = require('koa-router');
 const request = require('superagent');
+const Kyev = require('keyv');
 const Joi = require('joi');
 const schema = require('./schema');
 const render = require('../utils/render');
@@ -8,6 +9,9 @@ const sendEmail = require('../utils/send-email');
 const random = require('../utils/random');
 
 const router = new Router();
+const keyv = new keyv({
+    ttl: 1000 * 60 * 60 * 3
+});
 
 router.get('/', async (ctx, next) => {
   ctx.body = await render('password-reset.html', ctx.state);
@@ -17,6 +21,9 @@ router.get('/:code', async (ctx, next) => {
   // if code is invalid, redirect to `/password-reset`
   // Query database, find out which email this code is linked to.
   const code = ctx.params.code;
+
+  const info = keyv.get(code);
+  
   ctx.body = code;
 });
 
@@ -35,18 +42,25 @@ router.post('/', async (ctx, next) => {
      * @type {{email: string}}
      */
     const validated = await Joi.validate(body, schema.email);
+    const email = validated.email;
 
     debug('Input validated: %O', validated);
 
-    const resp = await request.get(`http://localhost:8000/is-taken?email=${validated.email}`)
+    const resp = await request.get(`http://localhost:8000/is-taken?email=${email}`)
       .auth(ctx.accessData.access_token, {type: 'bearer'})
 
     debug('Get response from api');
 
     const code = await random();
     const info = await sendEmail({
-      address: validate.email,
+      address: email,
       code
+    });
+
+    keyv.set(code, {
+      email,
+      name: 'name',
+      uuid: 'uuid'
     });
 
     debug('Email sent: %s', info.messageId);
