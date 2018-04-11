2fe101aff419fd9e704560a7911e4cbf330d391d
weiguo.ni <neefrankie@gmail.com>
2018-04-11 15:09:04 +0800

    rename debug.log to debug.info

--- utils/debug.js
+++ utils/debug.js
@@ -2,11 +2,11 @@ const debug = require('debug');
 
 module.exports = function(namespace) {
   const error = debug(namespace);
-  const log = debug(namespace);
-  log.log = console.log.bind(console);
+  const info = debug(namespace);
+  info.log = console.log.bind(console);
 
   return {
-    log,
+    info,
     error
   };
 }
