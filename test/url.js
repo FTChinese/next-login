615cb0c99d3ead9ad1c94fab43b80fba8e836d3e
weiguo.ni <neefrankie@gmail.com>
2018-04-11 15:10:27 +0800

    test url builder

---
+++ test/url.js
@@ -0,0 +1,16 @@
+const UrlBuilder = require('../utils/build-url');
+const test = require('ava');
+
+test('url', async t => {
+  const result = new UrlBuilder('http://www.ftchinese.com/callback')
+    .query({
+      a: 'foo',
+      b: 'bar'
+    })
+    .hash('redirect')
+    .toString();
+
+  console.log(result);
+
+  t.truthy(result);
+});
