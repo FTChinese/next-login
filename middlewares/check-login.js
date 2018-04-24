e659ed04d7011fefb27039837786a55cb44919ef
weiguo.ni <neefrankie@gmail.com>
2018-04-24 14:18:54 +0800

    rm check request path

--- middlewares/check-login.js
+++ middlewares/check-login.js
@@ -1,4 +1,5 @@
-const debug = require('debug')('user:check-login');
+const {escape} = require('querystring')
+const debug = require('../utils/debug')('user:check-login');
 
 // Those paths must be ignored in whatever cases prevent infinite loop:
 // A user is definitely not logged in if he goes to `/login`, and you check it and redirect hime to `/login`; before the page could be shown, he is checked again as not logged in, and redirect to login ...
@@ -10,26 +11,15 @@ const defaultIgnore = ['/login', '/signup', '/password-reset'];
  * @param  {type} ignorePaths=['/login'] Do not check if user logged in for those paths.
  * @return {Async Function}
  */
-function checkLogin(ignorePaths=[]) {
-  if (!Array.isArray(ignorePaths)) {
-    throw new Error('checkLogin only accepts an array of strings');
-  }
-
-  const ignore = new Set(defaultIgnore);
-
-  for (const elem of ignorePaths) {
-    if ('string' !== typeof elem) {
-      throw new Error('checkLogin only accepts an array of strings');
-    }
-    ignore.add(elem);
-  }
-
+function checkLogin() {
   return async (ctx, next) => {
+    debug.info('Accessing URL: %s', ctx.href);
+
     // Do nothing for `/favicon.ico`
     if (ctx.path == '/favicon.ico') return;
 
     if (isLoggedIn(ctx)) {
-      debug('Session data: %O', ctx.session);
+      debug.info('Session data: %O', ctx.session);
 
       ctx.state.user = {
         name: ctx.session.user.name,
@@ -37,28 +27,14 @@ function checkLogin(ignorePaths=[]) {
         verified: ctx.session.user.verified
       };
 
-      debug('userinfo: %O', ctx.state.user);
+      debug.info('userinfo: %O', ctx.state.user);
       return await next();
     }
 
-    debug('user not logged in');
+    ctx.state.user = null;
 
-    /**
-     * @type {Object | null} userinfo
-     * @property {string} name
-     * @property {number} id
-     */
-    ctx.state.userinfo = null;
-
-    debug("Path to ignore: %o", ignore);
-
-    if (ignore.has(ctx.path)) {
-      return await next();
-    }
-
-    // If current page is not in hte ignore list, redirect to login.
-    debug('Redirecting to login...');
-    ctx.redirect('/login');
+    debug.info('User not logged in. Redirecting to /login');
+    ctx.redirect(`/login?return_to=${escape(ctx.href)}`);
   }
 }
 
