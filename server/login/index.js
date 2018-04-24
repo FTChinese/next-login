ce5308165ba07cd7e1772a661db24453b7d62f0c
weiguo.ni <neefrankie@gmail.com>
2018-04-24 14:19:33 +0800

    rm wechat login for now

--- server/login/index.js
+++ server/login/index.js
@@ -1,7 +1,7 @@
 const debug = require('debug')('user:login');
 const Router = require('koa-router');
 const email = require('./email');
-const wechat = require('./wechat');
+// const wechat = require('./wechat');
 
 const router = new Router();
 
@@ -9,8 +9,8 @@ router.get('/', email.showPage);
 router.post('/', email.handleLogin);
 
 // Lauch Authorization Request
-router.get('/wechat', wechat.authRequest);
+// router.get('/wechat', wechat.authRequest);
 // Get Access Token Response
-router.post('/wechat/access', wechat.accessResponse);
+// router.post('/wechat/access', wechat.accessResponse);
 
 module.exports = router.routes();
