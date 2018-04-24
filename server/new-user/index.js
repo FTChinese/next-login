88f94de681aaa6c9dc99cc98577e2e82ddab40fa
weiguo.ni <neefrankie@gmail.com>
2018-04-24 14:19:58 +0800

    check login for email verification

--- server/new-user/index.js
+++ server/new-user/index.js
@@ -2,6 +2,7 @@ const Router = require('koa-router');
 const signup = require('./signup');
 const verify = require('./verify');
 const plan = require('./plan');
+const checkLogin = require('../../middlewares/check-login');
 
 const router = new Router();
 
@@ -9,7 +10,7 @@ router.get('/', signup.showPage);
 router.post('/', signup.handleCredentials);
 router.get('/plan', plan.showPage);
 router.post('/plan', plan.handlePlan);
-router.get('/verify/:token', verify.checkToken);
+router.get('/verify/:token', checkLogin(), verify.checkToken);
 
 // Later we might need to add ajax support to check if username or email is already being taken
 //
