f6c0f3b6ad371ef2b2a8f108de553d013b7545dc
weiguo.ni <neefrankie@gmail.com>
2018-06-20 04:59:38 +0800

    rm

--- server/new-user/schema.js
+++
@@ -1,6 +0,0 @@
-const Joi = require('joi');
-
-exports.login = Joi.object().keys({
-  email: Joi.string().trim().email().max(80).required(),
-  password: Joi.string().trim().min(8).max(20).required(),
-});
