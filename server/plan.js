39312d6f1ccc289bfefdb25e5bbf9a51ada179d2
weiguo.ni <neefrankie@gmail.com>
2018-06-20 18:28:41 +0800

    rm unused var

--- server/plan.js
+++ server/plan.js
@@ -1,16 +1,16 @@
 const request = require('superagent');
 const Router = require('koa-router');
-const render = require('../../utils/render');
-const debug = require('../../utils/debug')('user:plan');
-const endpoints = require('../../utils/endpoints');
+const render = require('../utils/render');
+const debug = require('../utils/debug')('user:plan');
+const endpoints = require('../utils/endpoints');
 
 const router = new Router();
 
-router.get('/', async (ctx, next) => {
+router.get('/', async (ctx) => {
   ctx.body = await render('new-user/plan.html', ctx.state);
 });
 
-router.post('/', async (ctx, next) => {
+router.post('/', async (ctx) => {
   const reqBody = ctx.request.body;
   debug.info(reqBody);
 
