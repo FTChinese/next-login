1bf0b93fb584bafe9d669085f1e8e01ecfa1e6b5
weiguo.ni <neefrankie@gmail.com>
2018-06-25 07:50:40 +0800

    fix bug of forgettign to await in case of not redirecting

--- middlewares/check-login.js
+++ middlewares/check-login.js
@@ -3,7 +3,9 @@ const debug = require('../utils/debug')('user:check-login');
 /**
  * checkLogin - This middleware will add userinfo to ctx.state
  *
- * `redirect` is used to prevent infinite loop when used on `/login` path itself.
+ * `redirect` is used to pages that want to use user session data but do not need to redirect.
+ * In inifinite loop will occur if you use `redirect=true` indiscrimnately on all path.
+ * Suppose you want to access `/profile` without login. This middleware will redirect you want to `/login`. And then when you are accessing `/login`, this middleware will again first check if you're loggedin. Certainly your are not. It again redirect you to `/login`, check login state again and redirect you to `/login`, indefinitely.
  * @return {Function}
  */
 function checkLogin({redirect=true}={}) {
@@ -36,8 +38,11 @@ function checkLogin({redirect=true}={}) {
 
       debug.info('User not logged in. Redirecting to %s', redirectTo);
   
-      ctx.redirect(redirectTo);
+      return ctx.redirect(redirectTo);
     }
+
+    // Remember to let the following middlewares to excute if users are not loggedin and you do not want to redirect away.
+    return await next();
   }
 }
 
