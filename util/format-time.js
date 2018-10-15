aa48bf377f6cf19266a6f034fafb1ad07c1460c3
weiguo.ni <neefrankie@gmail.com>
2018-10-15 18:45:31 +0800

    fix emtpy str

--- util/format-time.js
+++ util/format-time.js
@@ -4,5 +4,9 @@ const LAYOUT_ISO8601 = "YYYY-DD-MMTHH:mm:ssZ";
 
 const LAYOUT_CST = "YYYY年MM月DD日"
 exports.iso8601ToDate = function(str) {
+  if (!str) {
+    return "";
+  }
+  
   return moment(str).utcOffset(8).format(LAYOUT_CST);
 }
