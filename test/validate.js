373b628b4a6cba71adf5206519d4ed231995ebe7
weiguo.ni <neefrankie@gmail.com>
2018-12-17 21:11:32 +0800

    refactor validation

--- test/validate.js
+++ test/validate.js
@@ -1,19 +1,8 @@
 const test = require('ava');
-const Joi = require('joi');
-const schema = require('../server/schema');
-const {processJoiError} = require('../util/errors');
-const {validateLogin} = require("../util/validate")
-const credentials = {
-  email: 'foo',
-  password: '1234567'
-};
+const Chance = require("chance");
+const {validateLogin} = require("../util/validate");
 
-const profile = {
-  familyName: '',
-  givenName: '',
-  birthdate: '',
-  gender: 'female'
-};
+const chance = new Chance();
 
 /**
  * isJoi: true,
@@ -48,76 +37,16 @@ const profile = {
     } 
   }
  */
-test('credentials', async t => {
-  try {
-    const result = await Joi.validate(credentials, schema.account, {
-      abortEarly: false
-    });
-
-    console.log(result)
-  } catch (e) {
-    console.log("Error: %o", e);
-  }
-
-  t.pass();
-});
 
 test("login", async t => {
-  const {result, errors} = await validateLogin(credentials);
+  const login = {
+    email: chance.email(),
+    password: chance.character()
+  }
+  const {result, errors} = await validateLogin();
 
   console.log(result);
   console.log(errors);
 
   t.pass();
 });
-
-test('handleJoiErr', async t => {
-  try {
-    const result = await Joi.validate(credentials, schema.credentials, {
-      abortEarly: false
-    });
-
-    console.log(result)
-  } catch (e) {
-    console.log("Formatted error: %o", processJoiError(e));
-  }
-
-  t.pass();
-});
-
-test('profile', async t => {
-  const result = schema.profile.validate(profile, {abortEarly: false})
-
-  if (result.error) {
-    console.log(result.error);
-  }
-
-  console.log(result.value);
-
-  t.pass();
-});
-
-test('joi', async t => {
-  const data = {
-    customer: {
-      income: 100
-    }
-  }
-
-  const schema = {
-    customer: {
-      income: Joi.number().min(500)
-    }
-  };
-
-  const result = Joi.validate(data, schema);
-
-  if (result.error) {
-    console.log("Validation error: %o", result.error);
-  }
-
-  console.log(result.value);
-
-  t.pass()
-});
-
--- util/validate.js
+++ util/validate.js
@@ -1,5 +1,19 @@
-const Joi = require('joi');
+const Joi = require("joi");
+const debug = require("debug")("validate");
 
+const messages = {
+  "email_any.required": "邮箱不能为空",
+  "email_string.min": "",
+  "email_string.max": "",
+  "email_string.email": "不是有效的邮箱地址",
+  "password_any.required": "密码不能为空",
+  "password_string.min": "密码长度太短(最少8个字符)",
+  "password_string.max": "密码太长了",
+  "confirmPassword_any.required": "确认密码不能为空",
+  "confirmPassword_string.min": "确认密码长度太短(最少8个字符)",
+  "confirmPassword_string.max": "确认密码太长了",
+  "confirmPassword_mismatched": "两次输入的密码不符，请重新输入"
+}
 /**
  * 
  * @param {Joi.ValidationError} err 
@@ -7,8 +21,12 @@ const Joi = require('joi');
  */
 function buildError(err) {
   const errors = {};
+
+  console.log(err.details);
+
   for (item of err.details) {
     const key = item.path.join("_");
+
     const msgKey = `${key}_${item.type}`
     errors[key] = item.message;
   }
@@ -20,8 +38,8 @@ function buildError(err) {
  */
 exports.validateLogin = async function(credentials) {
   const schema = Joi.object().keys({
-    email: Joi.string().trim().email().min(3).max(254).required(),
-    password: Joi.string().trim().min(8).max(128).required()
+    email: Joi.string().trim().email().min(3).max(256).required(),
+    password: Joi.string().trim().max(128).required()
   });
 
   try {
@@ -35,6 +53,8 @@ exports.validateLogin = async function(credentials) {
       errors: null
     };
   } catch (e) {
+    debug("%O", e);
+
     if (!e.isJoi) {
       throw e;
     }
