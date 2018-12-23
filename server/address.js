7ba5c8f830971ac75988a4d72d48fdd8176b7a0e
weiguo.ni <neefrankie@gmail.com>
2018-12-23 23:35:44 +0800

    address router

--- server/address.js
+++ server/address.js
@@ -1,11 +1,11 @@
-const Router = require('koa-router');
 const request = require('superagent');
-const schema = require('./schema');
-
-const debug = require('../util/debug')('user:address');
+const Router = require('koa-router');
+const debug = require("debug")('user:address');
 const render = require('../util/render');
-const endpoints = require('../util/endpoints');
-const {processJoiError, processApiError, buildAlertDone} = require('../util/errors');
+const { nextApi } = require("../lib/endpoints");
+const { AddressValidator } = require("../lib/validate");
+const sitemap = require("../lib/sitemap");
+const { isAPIError, buildApiError } = require("../lib/response");
 
 const router = new Router();
 
@@ -13,7 +13,7 @@ const router = new Router();
 router.get('/', async (ctx) => {
   const userId = ctx.session.user.id;
 
-  const resp = await request.get(endpoints.profile)
+  const resp = await request.get(nextApi.profile)
     .set('X-User-Id', userId);
 
   /**
@@ -21,7 +21,6 @@ router.get('/', async (ctx) => {
    */
   const profile = resp.body;
   const address = profile.address;
-  debug.info('User address: %O', address);
 
   ctx.state.address = address;
 
@@ -31,34 +30,54 @@ router.get('/', async (ctx) => {
 // Update address
 router.post('/', async (ctx, next) => {
 
-  const result = schema.address.validate(ctx.request.body.address, {abortEarly: false});
-  if (result.error) {
-    ctx.session.errors = processJoiError(result.error);
-    
-    return ctx.redirect(ctx.path);
-  }
-
   /**
-   * @type {Address}
+   * @type {{province: string, city: string, district: string, postcode: string}}
    */
-  const address = result.value;
+  const address = ctx.request.body.address;
+  
+  const { result, errors } = new AddressValidator(address).validate();
+
+  if (errors) {
+    ctx.state.errors = errors;
+    ctx.state.address = address;
+
+    return await next();
+  }
 
   try {
 
     const userId = ctx.session.user.id;
 
-    await request.patch(endpoints.address)
+    await request.patch(nextApi.address)
       .set('X-User-Id', userId)
       .send(address);
 
-    ctx.session.alert = buildAlertDone('address_saved')
+    ctx.session.alert = {
+      done: "address_saved"
+    };
 
-    return ctx.redirect(ctx.path);
+    return ctx.redirect(sitemap.address);
 
   } catch (e) {
-    ctx.session.errors = processApiError(e)
+    ctx.state.address = address;
 
-    return ctx.redirect(ctx.path);
+    if (!isAPIError(e)) {
+      debug("%O", e);
+      ctx.state.errors = {
+        server: e.message
+      };
+
+      return await next();
+    }
+
+    /**
+     * @type {{message: string, error: Object}}
+     */
+    const body = e.response.body;
+
+    ctx.state.errors = buildApiError(body);
+
+    return await next();
   }
 });
 
