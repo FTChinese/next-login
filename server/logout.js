391bb1ad62cc036c73810cb412e825a3702c2577
weiguo.ni <neefrankie@gmail.com>
2018-12-23 23:36:07 +0800

    refactor

--- server/logout.js
+++ server/logout.js
@@ -1,13 +1,11 @@
-const debug = require('../util/debug')('login:server');
 const Router = require('koa-router');
+const sitemap = require("../lib/sitemap");
 
 const router = new Router();
 
 router.get('/', async (ctx, next) => {
   ctx.session = null;
-  const redirectTo = ctx.state.sitemap.login;
-  debug.info('Logout. Redirect to: %s', redirectTo);
-  ctx.redirect(redirectTo);
+  ctx.redirect(sitemap.login);
   return;
 });
 
