accdf8ba62b2cd936bada9d79ae9379c677de09e
weiguo.ni <neefrankie@gmail.com>
2018-12-24 15:30:33 +0800

    test

--- test/timeformatter.js
+++ test/timeformatter.js
@@ -1,8 +1,10 @@
 const test = require('ava');
-const formatter = require('../util/format-time');
+const { parseDateTime } = require("../lib/date-time");
 
-test('cst', async t => {
-  console.log(formatter.iso8601ToDate("2018-09-26T07:49:34Z"));
+test("parse", async t => {
+  const mmt = parseDateTime("2018-12-24");
 
+  console.log(mmt);
+  
   t.pass();
 });
