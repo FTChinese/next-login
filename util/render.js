7d9993f374ce7a0b8fb64c9c527466fd39c37013
weiguo.ni <neefrankie@gmail.com>
2018-12-24 15:41:13 +0800

    localize text in template engine

--- util/render.js
+++ util/render.js
@@ -1,7 +1,10 @@
 const path = require('path');
 const nunjucks = require('nunjucks');
 const util = require('util');
-nunjucks.configure(
+const numeral = require("numeral");
+const { localized } = require("../lib/membership");
+
+const env = nunjucks.configure(
   [
     path.resolve(__dirname, '../view'),
     path.resolve(__dirname, '../client')
@@ -11,5 +14,20 @@ nunjucks.configure(
     watch: process.env.NODE_ENV === 'development'
   }
 );
+
+/**
+ * Conert a number to currency string.
+ */
+env.addFilter("toCurrency", function(num) {
+  return numeral(num).format("0,0.00");
+});
+
+env.addFilter("localize", function(key) {
+  if (localized.hasOwnProperty(key)) {
+    return localized[key];
+  }
+
+  return key;
+});
 
 module.exports = util.promisify(nunjucks.render);
