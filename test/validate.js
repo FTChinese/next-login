4e3f2975cb5de01b01ab5f4fb7ccd94a6ea9baaa
weiguo.ni <neefrankie@gmail.com>
2018-12-25 17:01:39 +0800

    rm ava

--- test/validate.js
+++ test/validate.js
@@ -1,52 +1,17 @@
 const test = require('ava');
 const Chance = require("chance");
-const {validateLogin} = require("../util/validate");
+const validator = require('validator');
 
 const chance = new Chance();
 
-/**
- * isJoi: true,
-  name: 'ValidationError',
-  details: [ 
-    { message: '"email" must be a valid email',
-       path: [ 'email'],
-       type: 'string.email',
-       context: { value: 'foo', key: 'email', label: 'email' } 
-    },
-    { 
-      message: '"password" length must be at least 8 characters long',
-      path: [ 'password' ],
-      type: 'string.min',
-      context: { 
-        limit: 8,
-        value: '1234567',
-        encoding: undefined,
-        key: 'password',
-        label: 'password' 
-      } 
-    },
-  _object: { 
-    email: 'foo', 
-    password: '1234567' 
-  },
-  annotate: { 
-    [Function]
-     [length]: 1,
-     [name]: '',
-     [prototype]: { [constructor]: [Circular] } 
-    } 
-  }
- */
+test("isLength", async t => {
+  const str = chance.string({ length: 5 });
 
-test("login", async t => {
-  const login = {
-    email: chance.email(),
-    password: chance.character()
-  }
-  const {result, errors} = await validateLogin();
+  const isNotLen = !validator.isLength(str, {max: 50});
 
-  console.log(result);
-  console.log(errors);
+  console.log(isNotLen);
 
   t.pass();
 });
+
+
