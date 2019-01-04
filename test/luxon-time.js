9f424146c180bb73b65026271156391c1cd05cdf
weiguo.ni <neefrankie@gmail.com>
2019-01-04 08:07:11 +0800

    test

--- test/luxon-time.js
+++ test/luxon-time.js
@@ -1,7 +1,10 @@
 const { DateTime, Info } = require("luxon");
+const { parse, format } = require("date-fns");
 
 console.log(DateTime.local().toFormat("yyyy年LL月dd日 HH:mm:ss"));
 
-console.log(DateTime.fromISO("2017-05-15T08:30:00Z").setZone("Asia/Shanghai").toFormat("yyyy年LL月dd日 HH:mm:ss"));
+console.log(DateTime.fromISO("2017-05-15T08:30:00Z").toFormat("yyyy年LL月dd日 HH:mm:ss"));
 
 console.log(Info.features().zones);
+
+console.log(format(parse("2017-05-15T08:30:00Z"), "YYYY年MM月DD日 HH:mm:ss"));
