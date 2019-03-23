67f8b4f19912719f95a8dce23704b094a3ffbde4
weiguo.ni <neefrankie@gmail.com>
2019-03-23 19:13:37 +0800

    rm

--- util/connect-redis.js
+++
@@ -1,7 +0,0 @@
-const Redis = require('ioredis');
-// redis://user:password@hostname:port/db
-const redisUri = process.env.NODE_ENV === 'production' ? process.env.REDIS_URI : 'redis://@localhost:6379/';
-
-module.exports = function(options) {
-  return new Redis(redisUri, options);
-};
