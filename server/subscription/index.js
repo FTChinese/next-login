aaf396966f5c882d9ceffd6065688ef627a8f1e3
weiguo.ni <neefrankie@gmail.com>
2019-03-29 16:27:12 +0800

    add alipay callback

--- server/subscription/index.js
+++ server/subscription/index.js
@@ -12,6 +12,10 @@ const defaultPaywall = require("../../model/paywall-default.json");
 
 const payRouter = require("./pay");
 
+const {
+  sitemap,
+} = require("../../model/sitemap");
+
 const router = new Router();
 
 /**
@@ -34,6 +38,22 @@ router.get('/', async (ctx, next) => {
   }
 });
 
+router.get("/test", async (ctx, next) => {
+  const accountWrapper = new Account(ctx.session.user);
+  const account = await accountWrapper.fetchAccount();
+
+  debug("Account: %O", account);
+
+  ctx.state.account = account;
+  ctx.state.products = defaultPaywall.products;
+
+  ctx.body = await render('subscription/membership-test.html', ctx.state);
+
+  if (account && account.hasOwnProperty("id")) {
+    ctx.session.user = account;
+  }
+});
+
 /**
  * @description Show orders
  * /user/subscription/orders
@@ -49,6 +69,34 @@ router.get("/orders", async (ctx, enxt) => {
   ctx.body = await render("subscription/orders.html", ctx.state)
 });
 
+/**
+ * @description handle payment
+ * /user/subscription/pay/standard/month
+ * /user/subscription/pay/standard/year
+ * /user/subscription/pay/premium/month
+ */
 router.use("/pay", payRouter);
+
+/**
+ * @description Handle pay result
+ * /user/subscription/alipay/callback
+ */
+router.get("/alipay/callback", async (ctx, next) => {
+  /**
+   * @type {{error: string, error_description}}
+   */
+  const query = ctx.query;
+  if (query.error) {
+    ctx.state.errors = {
+      message: query.error_description || query.error
+    }
+
+    ctx.body = await render("subscription/alipay-result.html", ctx.state);
+
+    return;
+  }
+
+  ctx.redirect(sitemap.subs);
+});
 
 module.exports = router.routes();
