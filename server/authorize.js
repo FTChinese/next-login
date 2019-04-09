0772339fb4a83beccaad440ff4c662aa11fd3cbb
weiguo.ni <neefrankie@gmail.com>
2019-04-09 16:49:14 +0800

    authorize endpoint

--- server/authorize.js
+++ server/authorize.js
@@ -1,4 +1,8 @@
 const Router = require('koa-router');
+const debug = require("debug")("user:authorize");
+const {
+  OAuthClient,
+} = require("../lib/oauth-client")
 
 const router = new Router();
 
@@ -7,7 +11,25 @@ const router = new Router();
  * /authorize?response_type=code&client_id=xxxx&redirect_uri=xxx&state=xxx
  */
 router.get('/', async (ctx, next) => {
-  
+  /**
+   * @type {IOAuthReq}
+   */
+  const query = ctx.request.query;
+
+  // Validte query parameters
+
+  const client = new OAuthClient(query);
+
+  /**
+   * @type {code: string}
+   */
+  const granted = await client.requestCode(ctx.session.user);
+
+  debug("Granted code: %O", granted);
+
+  const redirectTo = client.buildRedirect(granted.code);
+
+  ctx.redirect(redirectTo);
 });
 
 module.exports = router.routes();
