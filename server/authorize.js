dded8d12c1ba1774715b54bf7c240868742ac937
weiguo.ni <neefrankie@gmail.com>
2019-04-09 17:58:34 +0800

    redirect to login if user not logged in

--- server/authorize.js
+++ server/authorize.js
@@ -1,8 +1,23 @@
+const {
+  URL,
+  URLSearchParams,
+} = require("url");
 const Router = require('koa-router');
 const debug = require("debug")("user:authorize");
 const {
   OAuthClient,
-} = require("../lib/oauth-client")
+} = require("../lib/oauth-client");
+const {
+  isLoggedIn
+} = require("./middleware");
+const {
+  sitemap,
+} = require("../lib/sitemap");
+const {
+  isProduction,
+} = require("../lib/config");
+
+const ftaHostname = "www.ftacademy.cn";
 
 const router = new Router();
 
@@ -10,26 +25,51 @@ const router = new Router();
  * @description Handle OAuth redirect request.
  * /authorize?response_type=code&client_id=xxxx&redirect_uri=xxx&state=xxx
  */
-router.get('/', async (ctx, next) => {
-  /**
-   * @type {IOAuthReq}
-   */
-  const query = ctx.request.query;
+router.get('/', 
+  async (ctx, next) => {
 
-  // Validte query parameters
+    if (isProduction) {
+      const srcUrl = new URL(ctx.header["referer"]);
+      if (srcUrl.hostname !== ftaHostname) {
+        ctx.state = 404;
+        return;
+      }
+    }
 
-  const client = new OAuthClient(query);
+    if (isLoggedIn(ctx)) {
+      ctx.state.user = ctx.session.user;
+      return await next();
+    }
 
-  /**
-   * @type {code: string}
-   */
-  const granted = await client.requestCode(ctx.session.user);
+    const query = ctx.request.query;
+    const params = new URLSearchParams(query);
+    const redirectTo = `${sitemap.login}?${params.toString()}`;
 
-  debug("Granted code: %O", granted);
+    ctx.redirect(redirectTo);
+    return;
+  },
 
-  const redirectTo = client.buildRedirect(granted.code);
+  async (ctx, next) => {
+    /**
+     * @type {IOAuthReq}
+     */
+    const query = ctx.request.query;
 
-  ctx.redirect(redirectTo);
-});
+    // Validte query parameters
+
+    const client = new OAuthClient(query);
+
+    /**
+     * @type {code: string}
+     */
+    const granted = await client.requestCode(ctx.session.user);
+
+    debug("Granted code: %O", granted);
+
+    const redirectTo = client.buildRedirect(granted.code);
+
+    ctx.redirect(redirectTo);
+  }
+);
 
 module.exports = router.routes();
