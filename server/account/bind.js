ded3d1136244b28e75a62893710b37b9ba99de85
weiguo.ni <neefrankie@gmail.com>
2019-04-15 16:16:42 +0800

    feat: data validation with joi

--- server/account/bind.js
+++ server/account/bind.js
@@ -1,17 +1,15 @@
 const Router = require('koa-router');
+const Joi = require("@hapi/joi");
 const debug = require("debug")('user:binding');
 const render = require('../../util/render');
 const {
   sitemap
 } = require("../../lib/sitemap");
 const {
-  isAPIError,
-  buildErrMsg,
+  errMessage,
   buildApiError,
+  ClientError,
 } = require("../../lib/response");
-const {
-  AccountValidtor
-} = require("../../lib/validate");
 
 const Credentials = require("../../lib/credentials");
 const Account = require("../../lib/account");
@@ -25,48 +23,90 @@ const {
 const {
   clientApp,
 } = require("../middleware");
+const {
+  joiOptions,
+  transformJoiErr,
+  loginSchema,
+  signUpSchema,
+} = require("../schema");
 
 const router = new Router();
 
+/**
+ * @description Ask user to enter the email to bind to wechat.
+ * /account/bind/email
+ */
 router.get("/email", async (ctx, next) => {
   ctx.body = await render("account/email-exists.html", ctx.state);
 });
 
+/**
+ * @description Check if email exists.
+ * /account/bind/email
+ */
 router.post("/email", async(ctx, next) => {
-  /**
-   * @type {string}
-   */
+
   const email = ctx.request.body.email;
+  const { error, value } = Joi.validate(
+    { email }, 
+    Joi.object().keys({
+      email: Joi.string().trim().email().required(), 
+    }),
+    joiOptions
+  );
+
+  if (error) {
+    debug("Validation error: %O", error);
+    ctx.state.errors = transformJoiErr(error.details);
+    ctx.state.email = email;
+    ctx.body = await render("account/email-exists.html", ctx.state);
+    return
+  }
+
+  debug("Validation result: %O", value);
 
   const credentials = new Credentials({
-    email,
+    email: value.email,
   });
 
   const exists = await credentials.emailExists();
 
-  ctx.session.email = email;
+  ctx.session.email = value.email;
 
+  // If email already exists, redirect to login
   if (exists) {
     ctx.redirect(sitemap.bindLogin);
     return;
   }
 
+  // If email does not exist, redirect to signup.
   ctx.redirect(sitemap.bindSignUp);
 });
 
+/**
+ * @description Ask user to enter password to log in.
+ * /account/bind/login
+ */
 router.get("/login", async(ctx, next) => {
+  // Retrieve the email previsouly same in session.
   const email = ctx.session.email;
+  
+  // If email does not exist, the session is expired.
   if (!email) {
     ctx.status = 404;
     return;
   }
 
+  // Show the email user previously entered.
   ctx.state.credentials = {
     email,
   }
 
   ctx.body = await render("account/login.html", ctx.state);
 
+  // Delete the session data for email in production
+  // so that it could only be used once, which means
+  // refreshing is not allowed.
   if (isProduction) {
     delete ctx.session.email;
   }
@@ -75,8 +115,10 @@ router.get("/login", async(ctx, next) => {
 /**
  * @description If email exists, let user login.
  * After logged in, compare ftc account and wechat account, and then perform mergind accordingly.
+ * /account/bind/login
  */
 router.post("/login",
+  // This is used to record login history.
   clientApp(),
 
   async(ctx, next) => {
@@ -85,19 +127,65 @@ router.post("/login",
      */
     const input = ctx.request.body.credentials;
 
-    const credentials = new Credentials(input);
+    const { value, error } = Joi.validate(
+      input,
+      loginSchema,
+      joiOptions,
+    );
 
-    const ftcId = await credentials.authenticate(ctx.state.clientApp);
+    if (error) {
+      ctx.state.errors = transformJoiErr(error.details);
+      ctx.state.credentials = input;
+      
+      return await next();
+    }
 
-    ctx.session.uid = ftcId;
+    try {
+      const credentials = new Credentials(value);
 
-    ctx.redirect(sitemap.bindMerge);
+      const ftcId = await credentials.authenticate(ctx.state.clientApp);
+  
+      ctx.session.uid = ftcId;
+  
+      ctx.redirect(sitemap.bindMerge);
+    } catch (e) {
+      ctx.state.credentials = input;
+
+      const clientErr = new ClientError(e);
+
+      if (!clientErr.isFromAPI()) {
+        ctx.state.errors = clientErr.buildGenericError();
+
+        return await next();
+      }
+
+      switch (e.status) {
+        case 404:
+        case 403:
+          ctx.state.errors = {
+            credentials: errMessage.credentials_invalid,
+          };
+          break;
+
+        default:
+          ctx.state.errors = buildApiError();
+          break;
+      }
+
+      debug("Errors: %O", ctx.state.errors);
+
+      return await next();
+    }
+  },
+
+  async (ctx) => {
+    ctx.body = await render("account/login.html", ctx.state);
   }
 );
 
 /**
- * @description
- * 
+ * @description Show user the accounts to be merged.
+ * /account/bind/merge
  */
 router.get("/merge", async(ctx, next) => {
 
@@ -168,6 +256,7 @@ router.get("/merge", async(ctx, next) => {
  * @description Perform accounts merging.
  * ftcId
  * wxId
+ * /account/bind/merge
  */
 router.post("/merge", async(ctx, next) => {
   const userId = ctx.request.body.userId;
@@ -198,13 +287,17 @@ router.post("/merge", async(ctx, next) => {
   }
 
   if (done) {
-    ctx.redirect(sitemap.profile);
+    ctx.redirect(sitemap.account);
     return;
   }
 
   throw new Error("Unknown error. Please try later.");
 });
 
+/**
+ * @description Ask user to enter password to create account.
+ * /account/bind/signup
+ */
 router.get("/signup", async(ctx, next) => {
   const email = ctx.session.email;
   if (!email) {
@@ -232,7 +325,51 @@ router.post("/signup",
   clientApp(),
 
   async(ctx, next) => {
+/**
+     * @type {ICredentials}
+     */
+    const input = ctx.request.body.credentials;
 
+    const { value, error } = Joi.validate(
+      input,
+      signUpSchema,
+      joiOptions,
+    );
+
+    if (error) {
+      ctx.state.errors = transformJoiErr(error.details);
+      ctx.state.credentials = input;
+      return await next();
+    }
+
+    try {
+
+      const wxUser = new WxUser(ctx.session.user.unionId);
+
+      const ftcId = await wxUser.signUp(value, ctx.state.clientApp);
+  
+      debug("Wx singup: %s", ftcId);
+  
+      ctx.redirect(sitemap.account);
+    } catch (e) {
+      ctx.state.credentials = input;
+
+      const clientErr = new ClientError(e);
+
+      if (!clientErr.isFromAPI()) {
+        ctx.state.errors = clientErr.buildGenericError();
+
+        return await next();
+      }
+
+      ctx.state.errors = clientErr.buildAPIError();
+
+      return await next();
+    }
+  },
+
+  async(ctx) => {
+    ctx.body = await render("account/signup.html", ctx.state);
   }
 );
 
