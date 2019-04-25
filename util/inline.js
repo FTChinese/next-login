9ad5ce4aaa9fbd2b7002af35b73d994f6d2fc625
weiguo.ni <neefrankie@gmail.com>
2019-04-25 16:32:20 +0800

    feat: inline css and js

--- util/inline.js
+++ util/inline.js
@@ -2,19 +2,37 @@ const { inlineSource } = require("inline-source");
 const path = require("path");
 const fs = require("fs").promises;
 
-const script =`<script inline src="build/outputs/bundle.min.js"></script>`
-const style = `<link rel="stylesheet" href="build/outputs/bundle.min.css">`
+const script =`<script inline src="build/production/main.js"></script>`
+const style = `<link inline rel="stylesheet" href="build/production/main.css">`
 
-async function inline() {
-  const scriptHtml = await inlineSource(script);
+async function inlineJs() {
+  const html = await inlineSource(script);
 
-  await fs.writeFile(path.resolve(__dirname, "../view/assets/js.min.html"), scriptHtml);
+  const dest = path.resolve(__dirname, "../view/assets/script.html");
+
+  console.log(`Wrting file to ${dest}`);
+
+  await fs.writeFile(dest, html);
+}
+
+async function inlineCss() {
+  const html = await inlineSource(style);
+
+  const dest = path.resolve(__dirname, "../view/assets/style.html");
+
+  console.log(`Writing file to ${dest}`);
+
+  await fs.writeFile(dest, html);
 }
 
 if (require.main === module) {
-  inline().catch(err => {
+  Promise.all([
+    inlineJs(),
+    inlineCss(),
+  ])
+  .then(() => {
+    console.log("Inline finished");
+  }).catch(err => {
     console.error(err);
   });
 }
-
-module.exports = inline;
