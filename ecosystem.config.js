f56d062b736d9889eb8ae65d7ac596ae21a93ad1
weiguo.ni <neefrankie@gmail.com>
2019-06-28 13:53:18 +0800

    feat: how to

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,21 @@
 const path = require('path');
 const interpreter = path.resolve(process.env.HOME, 'n/n/versions/node/11.13.0/bin/node');
 
+/**
+## Deploy with PM2
+
+To deploy a node app with PM2, you need take two steps:
+
+1. Initialize the remote folder as specified in `path`:
+```
+pm2 deploy ecosystem.config.js <app_name> setup
+```
+
+2. Deploy Code:
+```
+pm2 deploy ecosystemconfig.js <app_name>
+```
+ */
 module.exports = {
   /**
    * Application configuration section
@@ -8,7 +23,7 @@ module.exports = {
    */
   apps : [
     {
-      name      : "next-user",
+      name      : "next-user-deprecated",
       script    : "./index.js",
       cwd: __dirname,
       interpreter: interpreter,
@@ -24,8 +39,8 @@ module.exports = {
         DEBUG: "user*"
       },
       max_restart: 10,
-      error_file: path.resolve(process.env.HOME, 'logs/next-user-err.log'),
-      out_file: path.resolve(process.env.HOME, 'logs/next-user-out.log')
+      error_file: path.resolve(process.env.HOME, 'logs/next-user-deprecated-err.log'),
+      out_file: path.resolve(process.env.HOME, 'logs/next-user-deprecated-out.log')
     }
   ],
   deploy: {
@@ -34,7 +49,7 @@ module.exports = {
       host: "nodeserver",
       ref: "origin/master",
       repo: "https://github.com/FTChinese/next-user.git",
-      path: "/home/node/next/next-user",
+      path: "/home/node/next/next-user-deprecated",
       "pre-setup": "node -v",
       "post-setup": "ls -la",
       "pre-deploy-local": "echo 'Begin to deploy'",
