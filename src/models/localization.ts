81edcecc0fd9d566bc10f20530004d3abbf90519
weiguo.ni <neefrankie@gmail.com>
2019-09-23 10:29:00 +0800

    feat: localization text

---
+++ src/models/localization.ts
@@ -0,0 +1,32 @@
+module.exports =  {
+    "year": "年",
+    "month": "月",
+    "CNY": "¥",
+    "standard": "标准会员",
+    "premium": "高端会员",
+    "tenpay": "微信支付",
+    "alipay": "支付宝",
+    "M": "男",
+    "F": "女",
+    "zeroMember": "尚未成为会员",
+    "wechat": "微信",
+};
+
+export const memberInfo = new Map<string, string>([
+    ["year", "年"],
+    ["month", "月"],
+    ["standard", "标准会员"],
+    ["premium", "高端会员"],
+    ["zeroMember", "尚未成为会员"],
+]);
+
+export const paymentMethods = new Map<string, string>([
+    ["wechat", "微信支付"],
+    ["alipay", "支付宝"],
+    ["stripe", "Stripe"],
+]);
+
+export const genders = new Map<string, string>([
+    ["M", "男"],
+    ["F", "女"],
+]);
