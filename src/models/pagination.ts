38181be8e2681702bb20b625b8b826d6aa52d528
weiguo.ni <neefrankie@gmail.com>
2019-09-23 16:51:20 +0800

    feat: pagination

---
+++ src/models/pagination.ts
@@ -0,0 +1,43 @@
+export interface IPagination {
+    page: number;
+    per_page: number;
+}
+
+export class Paging {
+    private currentPage: number;
+    private itemsPerPage: number;
+
+    private size: number
+
+    constructor(current: number = 1, perPage: number = 20) {
+        this.currentPage = current;
+        this.itemsPerPage = perPage;
+    }
+
+    setSize(s: number) {
+        this.size = s;
+    }
+
+    get previousPage(): number {
+        return this.currentPage - 1;
+    }
+
+    get nextPage(): number {
+        return this.currentPage + 1;
+    }
+
+    showPrevious(): boolean {
+        return this.currentPage > 1;
+    }
+
+    showNext(): boolean {
+        return this.size >= this.itemsPerPage;
+    }
+
+    toObject(): IPagination {
+        return {
+            page: this.currentPage,
+            per_page: this.itemsPerPage,
+        }
+    }
+}
