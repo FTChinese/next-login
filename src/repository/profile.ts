516dd36f4991748240ab916ed0a6f810117234ea
weiguo.ni <neefrankie@gmail.com>
2019-09-23 16:52:09 +0800

    feat: profile repository

---
+++ src/repository/profile.ts
@@ -0,0 +1,59 @@
+import request from "superagent";
+import {
+    TypedJSON
+} from "typedjson";
+import {
+    readerApi,
+    KEY_USER_ID,
+} from "../config/api";
+import { 
+    IProfile,
+    INameFormData,
+    IMobileFormData,
+    IAddress,
+} from "../models/reader";
+
+class ProfileRepo {
+
+    async fetchProfile(ftcId: string): Promise<IProfile> {
+        const resp = await request
+            .get(readerApi.profile)
+            .set(KEY_USER_ID, ftcId);
+
+        return resp.body;
+    }
+
+    async fetchAddress(ftcId: string): Promise<IAddress> {
+        const resp = await request
+            .get(readerApi.address)
+            .set(KEY_USER_ID, ftcId)
+
+        return resp.body;
+    }
+
+    async updateName(ftcId: string, data: INameFormData): Promise<boolean> {
+        const resp = await request
+            .patch(readerApi.name)
+            .set(KEY_USER_ID, ftcId)
+            .send(data);
+
+        return resp.noContent;
+    }
+
+    async updateMobile(ftcId: string, data: IMobileFormData): Promise<boolean> {
+        const resp = await request
+            .patch(readerApi.mobile)
+            .set(KEY_USER_ID, ftcId)
+            .send(data);
+
+        return resp.noContent;
+    }
+
+    async updateAddress(address: IAddress): Promise<boolean> {
+        const resp = await request
+            .patch(readerApi.address)
+            .send(address);
+
+        return resp.noContent;
+    }
+}
