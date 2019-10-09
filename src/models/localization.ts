23c841da9447c982304545d73fcb6d0ef6782eb1
weiguo.ni <neefrankie@gmail.com>
2019-10-09 17:41:40 +0800

    feat: use object to hold localization data

--- src/models/localization.ts
+++ src/models/localization.ts
@@ -1,24 +1,30 @@
-export const memberInfo = new Map<string, string>([
-    ["year", "年"],
-    ["month", "月"],
-    ["standard", "标准会员"],
-    ["premium", "高端会员"],
-    ["zeroMember", "尚未成为会员"],
-]);
+export const paymentMethods = {
+    "wechat": "微信支付",
+    "alipay": "支付宝",
+    "stripe": "Stripe",
+};
 
-export const paymentMethods = new Map<string, string>([
-    ["wechat", "微信支付"],
-    ["alipay", "支付宝"],
-    ["stripe", "Stripe"],
-]);
+export const genders = {
+    "M": "男",
+    "F": "女",
+};
 
-export const genders = new Map<string, string>([
-    ["M", "男"],
-    ["F", "女"],
-]);
+export const intervals = {
+    "year": "年",
+    "month": "月",
+};
 
 export const memberTypes = {
     "standard": "标准会员",
     "premium": "高端会员",
     "zeroMember": "尚未成为会员",
 };
+
+export const currencySymbols = new Map<string, string>([
+    ["cny", "¥"],
+    ["eur", "€"],
+    ["gbp", "£"],
+    ["hkd", "HK$"],
+    ["jpy", "¥"],
+    ["usd", "US$"],
+]);
