0cb039500ae23e0dd0eb7ddc8c04cb05b3d9ee82
weiguo.ni <neefrankie@gmail.com>
2019-10-11 11:58:33 +0800

    refactor: delete js files

--- server/verification/index.js
+++
@@ -1,9 +0,0 @@
-const Router = require('koa-router');
-
-const verifyEmail = require("./email");
-
-const router = new Router();
-
-router.use("/email", verifyEmail);
-
-module.exports = router.routes();
