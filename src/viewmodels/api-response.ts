f74be46a75384cb049db99b648d093305609bdef
weiguo.ni <neefrankie@gmail.com>
2019-10-14 17:37:44 +0800

    fix typo

--- src/viewmodels/api-response.ts
+++ src/viewmodels/api-response.ts
@@ -9,12 +9,12 @@ const log = debug("user:api-error");
 export const apiInvalidMessages = new Map<string, string>([
     // ["email_missing_field", "邮箱不能为空"],
     // ["email_invalid", "不是有效的邮箱地址"],
-    ["email_already_exists", "该邮箱已经注册FT中文网账号，请使用其他邮箱"],
+    // ["email_already_exists", "该邮箱已经注册FT中文网账号，请使用其他邮箱"],
     // ["email_not_found", "该邮箱不存在，请检查您输入的邮箱是否正确"],
     // ["email_token_not_found", "您使用了无效的邮箱验证链接"],
     // ["password_missing_field", "密码不能为空"],
     // ["password_invalid", "密码无效"],
-    ["password_token_invalid", "无法重置密码。您似乎使用了无效的重置密码链接，请重试"],
+    // ["password_token_invalid", "无法重置密码。您似乎使用了无效的重置密码链接，请重试"],
     // ["password_forbidden", "当前密码错误"],
     // ["passwords_mismatched", "两次输入的密码不符，请重新输入"],
     ["token_mising_field", "token不能为空"],
@@ -24,13 +24,13 @@ export const apiInvalidMessages = new Map<string, string>([
     ["userName_already_exists", "用户名已被占用，请使用其他用户名"],
     ["mobile_missing_field", "手机号码不能为空"],
     ["mobile_invalid", "手机号码无效"],
-    ["server_unauthorized", "访问被拒绝，无权进行此操作"],
+    // ["server_unauthorized", "访问被拒绝，无权进行此操作"],
     ["userId_missing_field", "请求中缺少'userId'字段"],
     // Gift card error response.
     ["redeem_code_not_found", "没有找到该卡号，请确认您输入的礼品卡号有效、没有过期、未被激活"],
     ["redeem_code_missing_field", "兑换码缺失"],
-    ["member_already_exists", "您已经是会员了"],
-    ["anchor_missing_field", "会员账号解绑必须选择哪个账号保留会员信息"],
+    // ["member_already_exists", "您已经是会员了"],
+    // ["anchor_missing_field", "会员账号解绑必须选择哪个账号保留会员信息"],
 ]);
 interface InvalidMsg {
     missing?: string;
@@ -55,7 +55,7 @@ const invalids: Dictionary<InvalidMsg> = {
     },
     newPassword: {
         missing_field: "新密码不能为空",
-        invalid: "新密阿妹无效",
+        invalid: "新密无效",
     },
 }
 
