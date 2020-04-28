83663d95a96207aa656c3c9fcd5e34b8b17e54c1
weiguo.ni <neefrankie@gmail.com>
2020-04-28 17:11:02 +0800

    feat: add svelte

--- client/rollup.config.js
+++ client/rollup.config.js
@@ -1,22 +1,71 @@
+import svelte from 'rollup-plugin-svelte';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import autoPreprocess from 'svelte-preprocess'
 import typescript from '@rollup/plugin-typescript';
 import { terser } from "rollup-plugin-terser";
+import buble from '@rollup/plugin-buble';
 
 const production = !process.env.ROLLUP_WATCH;
 
-export default {
-  input: "./client/script/main.ts",
-  output: {
-    file: "./build/script/main.js",
-    format: "iife",
-    sourcemap: true,
+export default [
+  // Process vanilla TS.
+  {
+    input: "./client/script/main.ts",
+    output: {
+      file: "./build/script/main.js",
+      format: "iife",
+      sourcemap: true,
+    },
+    plugins: [
+      // When using this plugin, tsconfig.json cannot have this option:  
+      // "outDir": "../build/ts-out"
+      // See https://github.com/rollup/plugins/issues/287
+      typescript({
+        tsconfig: './client/tsconfig.json' // relative to process.cwd().
+      }),
+      buble(),
+      production && terser(),
+    ],
   },
-  plugins: [
-    // When using this plugin, tsconfig.json cannot have this option:  
-    // "outDir": "../build/ts-out"
-    // See https://github.com/rollup/plugins/issues/287
-    typescript({
-      tsconfig: './client/tsconfig.json' // relative to process.cwd().
-    }),
-    production && terser(),
-  ],
-};
+  // Process svelte.
+  {
+    input: './client/src/main.js',
+    output: {
+      sourcemap: true,
+      format: 'iife',
+      name: 'app',
+      file: './build/script/reader-app.js'
+    },
+    plugins: [
+      svelte({
+        // enable run-time checks when not in production
+        dev: !production,
+        preprocess: autoPreprocess(),
+        // we'll extract any component CSS out into
+        // a separate file - better for performance
+        css: css => {
+          css.write('./build/style/reader-app.css');
+        }
+      }),
+
+      // If you have external dependencies installed from
+      // npm, you'll most likely need these plugins. In
+      // some cases you'll need additional configuration -
+      // consult the documentation for details:
+      // https://github.com/rollup/plugins/tree/master/packages/commonjs
+      resolve({
+        browser: true,
+        dedupe: ['svelte']
+      }),
+      commonjs(),
+    
+      // If we're building for production (npm run build
+      // instead of npm run dev), minify
+      production && terser()
+    ],
+    watch: {
+      clearScreen: false
+    }
+  }
+];
