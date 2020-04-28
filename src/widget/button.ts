5bdb6b3b4d31689c6fbbd693bfbb94110385bd85
weiguo.ni <neefrankie@gmail.com>
2020-04-28 20:55:09 +0800

    refactor: build html tag in js

--- src/widget/button.ts
+++ src/widget/button.ts
@@ -9,78 +9,72 @@ type BtnSize = "sm" | "lg" | "block";
  * Using Bootstrap class: btn btn-primary/btn-outline-primary btn-block
  */
 export class Button {
-    text: string = "";
-    type: BtnType = "submit";
-    disableWith?: string;
-    private style?: BtnStyle;
-    private size?: BtnSize;
-    private outline?: boolean;
+  private text: string = "";
+  private type: BtnType = "submit";
+  private disableWith?: string;
+  private style?: BtnStyle;
+  private size?: BtnSize;
+  private outline?: boolean;
 
-    setName(s: string): Button {
-        this.text = s;
-        return this;
+  setName(s: string): Button {
+    this.text = s;
+    return this;
+  }
+
+  setDisableWith(s: string): Button {
+    this.disableWith = s;
+    return this;
+  }
+
+  setType(t: BtnType): Button {
+    this.type = t;
+    return this;
+  }
+
+  setStyle(s: BtnStyle): Button {
+    this.style = s;
+    return this;
+  }
+
+  setSize(s: BtnSize): Button {
+    this.size = s;
+    return this;
+  }
+
+  setBlock(): Button {
+    this.size = "block";
+    return this;
+  }
+
+  render(): string {
+    const attrs = (new Attributes())
+      .set("type", this.type)
+      .addClassName("btn");
+
+    // A string like: btn-outline-primary or btn-primary
+    if (this.style) {
+      attrs.addClassName(`btn${this.outline ? '-outline' : '-'}${this.style}`)
     }
 
-    setDisableWith(s: string): Button {
-        this.disableWith = s;
-        return this;
+    // A string like btn-block
+    if (this.size) {
+      attrs.addClassName(`btn-${this.size}`);
     }
 
-    setType(t: BtnType): Button {
-        this.type = t;
-        return this;
+    if (this.disableWith) {
+      attrs.set("data-disable-with", this.disableWith)
     }
 
-    setStyle(s: BtnStyle): Button {
-        this.style = s;
-        return this;
-    }
+    return `<button ${attrs.build()}>${this.text}</button>`;
+  }
 
-    setSize(s: BtnSize): Button {
-        this.size = s;
-        return this;
-    }
+  static primary(): Button {
+    return new Button()
+      .setStyle("primary")
+  }
 
-    setBlock(): Button {
-        this.size = "block";
-        return this;
-    }
-
-    get classNames(): string {
-        const names = ["btn"];
-
-        // A string like: btn-outline-primary or btn-primary
-        if (this.style) {
-            names.push(`btn${this.outline ? '-outline' : '-'}${this.style}`)
-        }
-
-        // A string like btn-block
-        if (this.size) {
-            names.push(`btn-${this.size}`);
-        }
-
-        return names.join(' ');
-    }
-
-    get attributes(): string {
-        const attrs = new Attributes();
-        attrs.setClassNames(this.classNames)
-            .set("type", this.type)
-        
-        if (this.disableWith) {
-            attrs.set("data-disable-with", this.disableWith)
-        }
-
-        return attrs.build();
-    }
-
-    static primary(): Button {
-        return new Button()
-            .setStyle("primary")
-    }
-
-    static secondary(): Button {
-        return new Button()
-            .setStyle("secondary");
-    }
+  static secondary(): Button {
+    return new Button()
+      .setStyle("secondary");
+  }
 }
