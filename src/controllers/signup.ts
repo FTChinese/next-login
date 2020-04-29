f68441c838c29ba17a33e88e3eafedeb78ecd1c8
weiguo.ni <neefrankie@gmail.com>
2020-04-29 13:47:35 +0800

    refactor

--- src/controllers/signup.ts
+++ src/controllers/signup.ts
@@ -1,21 +1,16 @@
 import Router from "koa-router";
 import render from "../util/render";
 import {
-    signUpViewModel,
-} from "../viewmodels/signup-viewmodel";
-import {
-    collectAppHeaders,
+  collectAppHeaders,
 } from "./middleware";
 import {
-    IHeaderApp,
+  IHeaderApp,
 } from "../models/header";
 import {
-    ISignUpFormData,
-} from "../viewmodels/validator";
-import { 
-    profileMap 
+  profileMap
 } from "../config/sitemap";
 import { IOAuthSession, oauthServer } from "../models/ftc-oauth";
+import { SignUpPage, SignUpBuilder, SignUpData } from "../pages/signup";
 
 const router = new Router();
 
@@ -23,54 +18,61 @@ const router = new Router();
  * @description Show signup page.
  */
 router.get("/", async (ctx, next) => {
-    const uiData = signUpViewModel.buildUI();
+  const uiData = new SignUpPage(SignUpBuilder.default());
 
-    Object.assign(ctx.state, uiData);
+  Object.assign(ctx.state, uiData);
 
-    ctx.body = await render("signup.html", ctx.state);
+  ctx.body = await render("signup.html", ctx.state);
 });
 
 /**
  * @description Handle signup data.
  */
 router.post("/", collectAppHeaders(), async (ctx, next) => {
-    const formData: ISignUpFormData | undefined = ctx.request.body.credentials;
+  const formData: SignUpData | undefined = ctx.request.body.credentials;
 
-    if (!formData) {
-        throw new Error("form data not found");
-    }
+  if (!formData) {
+    throw new Error("form data not found");
+  }
 
-    const headers: IHeaderApp = ctx.state.appHeaders;
+  const headers: IHeaderApp = ctx.state.appHeaders;
 
-    const { success, errForm, errResp } = await signUpViewModel.signUp(formData, headers);
+  const suBuilder = new SignUpBuilder(formData);
+  const isValid = await suBuilder.validate();
+  if (!isValid) {
+    const uiData = new SignUpPage(suBuilder);
+    Object.assign(ctx.state, uiData);
+    return await next();
+  }
 
-    if (!success) {
-        const uiData = signUpViewModel.buildUI(formData, { errForm, errResp });
+  const account = await suBuilder.create(headers);
+  if (!account) {
+    const uiData = new SignUpPage(suBuilder);
 
-        Object.assign(ctx.state, uiData);
+    Object.assign(ctx.state, uiData);
 
-        return await next();
-    }
+    return await next();
+  }
+
+  // @ts-ignore
+  ctx.session.user = success;
+
+  // @ts-ignore
+  if (ctx.session.oauth) {
+    // @ts-ignore
+    const oauthSession: IOAuthSession = ctx.session.oauth;
+
+    ctx.redirect(oauthServer.buildAuthorizeUrl(oauthSession));
 
     // @ts-ignore
-    ctx.session.user = success;
+    delete ctx.session.oauth;
 
-    // @ts-ignore
-    if (ctx.session.oauth) {
-        // @ts-ignore
-        const oauthSession: IOAuthSession = ctx.session.oauth;
-
-        ctx.redirect(oauthServer.buildAuthorizeUrl(oauthSession));
-
-        // @ts-ignore
-        delete ctx.session.oauth;
-        
-        return;
-    }
-    return ctx.redirect(profileMap.base);
+    return;
+  }
+  return ctx.redirect(profileMap.base);
 
 }, async (ctx, next) => {
-    ctx.body = await render('signup.html', ctx.state);
+  ctx.body = await render('signup.html', ctx.state);
 });
 
 export default router.routes();
