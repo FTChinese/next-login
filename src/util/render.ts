45b3fb3d601a7564fa8ad1866ea6a0cb92bbe0c2
weiguo.ni <neefrankie@gmail.com>
2020-06-06 23:09:42 +0800

    fix: filters

--- src/util/render.ts
+++ src/util/render.ts
@@ -1,13 +1,31 @@
 import { resolve } from "path";
 import { configure, render } from "nunjucks";
+import makrdown from "nunjucks-markdown";
+import marked from "marked";
+import { formatMoney, iso8601ToCST } from "./formatter";
 
-configure(
+const env = configure(
   [resolve(__dirname, "../../views"), resolve(__dirname, "../../client")],
   {
     noCache: process.env.NODE_ENV == "development",
     watch: process.env.NODE_ENV == "development",
   }
 );
+
+env.addFilter("toCurrency", function(num: number) {
+  return formatMoney(num);
+});
+
+env.addFilter("toCST", (str: string) => {
+  return iso8601ToCST(str);
+});
+
+marked.setOptions({
+  gfm: true,
+  breaks: true,
+})
+
+makrdown.register(env, marked);
 
 function promisifiedRender(name: string, context?: object): Promise<string> {
   return new Promise((resolve, reject) => {
