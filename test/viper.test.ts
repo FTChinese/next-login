179d7f2d68d57598bb7357eeb4e3a0e907ab785c
weiguo.ni <neefrankie@gmail.com>
2020-07-07 22:25:30 +0800

    removed legacy test file

--- test/viper.test.ts
+++
@@ -1,8 +0,0 @@
-import { viper } from "../src/config/viper";
-viper.setConfigPath(process.env.HOME)
-    .setConfigName("config/api.toml")
-    .readInConfig();
-
-test("config", () => {
-    console.log(viper.getConfig().api_url);
-});
