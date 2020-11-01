8b30ff49823f0ae69d5b8c952ae799850b47da39
neefrankie <neefrankie@gmail.com>
2020-11-01 12:09:06 +0800

    removed svelte

--- client/rollup.config.js
+++ client/rollup.config.js
@@ -1,7 +1,3 @@
-import svelte from 'rollup-plugin-svelte';
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import autoPreprocess from 'svelte-preprocess'
 import typescript from '@rollup/plugin-typescript';
 import { terser } from "rollup-plugin-terser";
 import buble from '@rollup/plugin-buble';
@@ -28,44 +24,4 @@ export default [
       production && terser(),
     ],
   },
-  // Process svelte.
-  {
-    input: './client/src/main.js',
-    output: {
-      sourcemap: true,
-      format: 'iife',
-      name: 'app',
-      file: './build/script/reader-app.js'
-    },
-    plugins: [
-      svelte({
-        // enable run-time checks when not in production
-        dev: !production,
-        preprocess: autoPreprocess(),
-        // we'll extract any component CSS out into
-        // a separate file - better for performance
-        css: css => {
-          css.write('./build/style/reader-app.css');
-        }
-      }),
-
-      // If you have external dependencies installed from
-      // npm, you'll most likely need these plugins. In
-      // some cases you'll need additional configuration -
-      // consult the documentation for details:
-      // https://github.com/rollup/plugins/tree/master/packages/commonjs
-      resolve({
-        browser: true,
-        dedupe: ['svelte']
-      }),
-      commonjs(),
-    
-      // If we're building for production (npm run build
-      // instead of npm run dev), minify
-      production && terser()
-    ],
-    watch: {
-      clearScreen: false
-    }
-  }
 ];
