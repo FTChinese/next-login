eda1d7004f551f76b9b9ea065822ed0aa9d3e0cf
weiguo.ni <neefrankie@gmail.com>
2021-02-01 16:42:15 +0800

    feat: client js for stripe

--- client/rollup.config.js
+++ client/rollup.config.js
@@ -24,4 +24,21 @@ export default [
       production && terser(),
     ],
   },
+  {
+    input: "./client/script/stripe-pay.ts",
+    output: {
+      file: "./build/script/stripe-pay.js",
+      format: "iife",
+      sourcemap: true,
+    },
+    plugins: [
+      // When using this plugin, tsconfig.json cannot have this option:  
+      // "outDir": "../build/ts-out"
+      // See https://github.com/rollup/plugins/issues/287
+      typescript({
+        tsconfig: './client/tsconfig.json' // relative to process.cwd().
+      }),
+      production && terser(),
+    ],
+  }
 ];
