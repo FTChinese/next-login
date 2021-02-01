51cccab4a91e45bcbf6c53f3afd8833d5f08f689
weiguo.ni <neefrankie@gmail.com>
2021-02-01 16:48:44 +0800

    feat: cache stripe prices

--- src/repository/cache.ts
+++ src/repository/cache.ts
@@ -1,13 +1,37 @@
 import NodeCache from 'node-cache';
 import { Paywall, Plan } from '../models/paywall';
 import { Edition } from '../models/enums';
+import { Price, StripeEdition } from '../models/stripe';
 
 const cache = new NodeCache({
   stdTTL: 7200,
 });
 
+// Used as the key to cache paywall.
 const KEY_PAYWALL_CACHE = 'paywall';
 
+function getEditionKey(e: Edition): string {
+  return `${e.tier}_${e.cycle}`;
+}
+
+function getStripeEditionKey(e: StripeEdition): string {
+  let prefix: string
+  if (e.live) {
+    prefix = 'live'
+  } else {
+    prefix = 'test'
+  }
+
+  return `${prefix}_${getEditionKey(e)}`;
+}
+
+function getModeStr(live: boolean): string {
+  if (live) {
+    return 'live'
+  }
+
+  return 'test'
+}
 class PaywallCache {
 
   savePaywall(p: Paywall) {
@@ -23,9 +47,13 @@ class PaywallCache {
     });
   }
 
-  savePlans(plans: Plan[]) {
+  // Cache plan with key set to <tier>_<cycle>:
+  // `standard_year`
+  // `standard_month`
+  // `premium_year`
+  saveFtcPlans(plans: Plan[]) {
     plans.forEach(plan => {
-      cache.set(`${plan.tier}_${plan.cycle}`, plan);
+      cache.set(`ftc_${getEditionKey(plan)}`, plan);
     });
   }
 
@@ -33,8 +61,20 @@ class PaywallCache {
     return cache.get<Paywall>(KEY_PAYWALL_CACHE);
   }
 
-  getPlan(edition: Edition): Plan | undefined {
-    return cache.get<Plan>(`${edition.tier}_${edition.cycle}`);
+  getFtcPlan(edition: Edition): Plan | undefined {
+    return cache.get<Plan>(`ftc_${getEditionKey(edition)}`);
+  }
+
+  saveStripePrices(prices: Price[]) {
+    prices.forEach(price => {
+      if (price.active) {
+        cache.set(`stripe_${getStripeEditionKey(price)}`, price);
+      }
+    });
+  }
+
+  getStripePrice(edition: StripeEdition): Price | undefined {
+    return cache.get<Price>(`stripe_${getStripeEditionKey(edition)}`);
   }
 
   clear() {
